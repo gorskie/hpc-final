48d101e0e2e3b5273d3e9413f6e1b923ee47eceb
eric <gorskie@moravian.edu>
2021-05-03 14:26:16 -0400

    working print-graph.py

--- print-graph.py
+++ print-graph.py
@@ -1,10 +1,21 @@
-import os
 import numpy as npy
 import matplotlib.pyplot as plt
 
-os.chdir('/Users/eric/hpc/projects/hpc-final/')
-m = npy.load('output-1d-s.npy')
-plt.plot(m.T)
-#plt.imshow(m)
-#plt.colorbar()
+grid_size = (100, 100)
+
+m = npy.load('output-2d-s.npy')
+m = m.reshape(-1, *grid_size)
+
+x = 1000000 #  abs(m).max()
+
+plt.ion()
 plt.show()
+
+for t, grid in enumerate(m):
+    plt.clf()
+    plt.title("Timestep "+str(t))
+    plt.imshow(grid, aspect='auto', cmap='seismic')
+    plt.colorbar()
+    plt.clim([-x, x])
+    plt.draw()
+    plt.pause(0.2)
