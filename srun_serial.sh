31768ae23773048671a73a1648960e6c1affa2a7
eric <gorskie@moravian.edu>
2021-05-04 15:42:38 -0400

    added working sbatch files and changes to transverse output location

--- srun_serial.sh
+++ srun_serial.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/bash
-#SBATCH -p shared
+#SBATCH -p compute
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=6
-#SBATCH --mem=10G
-#SBATCH -t 12:00:00
-#SBATCH -A mor100
 #SBATCH -o %x.%j.out
 #SBATCH -e %x.%j.err
 #SBATCH --export=ALL
@@ -14,17 +11,30 @@
 
 SCRATCH="/scratch/$USER/job_$SLURM_JOB_ID"
 
+cp "$HOME/hpc-final/matrix.c" "$SCRATCH"
+cp "$HOME/hpc-final/matrix.h" "$SCRATCH"
+cp "$HOME/hpc-final/matrix_io_helpers.h" "$SCRATCH"
+cp "$HOME/hpc-final/util.c" "$SCRATCH"
+cp "$HOME/hpc-final/util.h" "$SCRATCH"
+
 for PROGNAME in transverse additive interval; do
-    echo $PROGNAME
-    cp "$HOME/2d-fdtd-$PROGNAME-s.c" "$SCRATCH"
+    echo 2d-fdtd-$PROGNAME-s
+    cp "$HOME/hpc-final/2d-fdtd-$PROGNAME-s.c" "$SCRATCH"
     SFILE="$SCRATCH/2d-fdtd-$PROGNAME-s.c"
-    for MESH_SIZE in {50..100..10}
-    do  
-        echo $MESH_SIZE
-        gcc -Wall -g -O3 -march=native 2d-fdtd-$PROGNAME-s.c matrix.c util.c -o 2d-fdtd-$PROGNAME-s -lm -DMESH_SIZE=$MESH_SIZE
-        FNAME="output-fdtd-2d-$FILE-$MESH_SIZE-s.npy"
-        ./2d-fdtd-$PROGNAME-s "$FNAME" &  # & means to background the process
+    TIMESTEPS=100
+    for MESH_SIZE in {100..1000..$TIMESTEPS} # allowed?
+    # for MESH_SIZE in {50..100..10}
+    do
+        echo "SIZE: $MESH_SIZE"
+        echo "TIMESTEPS: $TIMESTEPS"
+        gcc -Wall -g -O3 -march=native $SFILE matrix.c util.c -o $SCRATCH/2d-fdtd-$PROGNAME-s -lm -DMESH_SIZE=$MESH_SIZE -DNUM_TIMESTEPS=$TIMESTEPS -DSAVE_EVERY_N_STEPS=2
+        FNAME="output-fdtd-2d-$PROGNAME-$MESH_SIZE-s.npy"
+        "$SCRATCH/2d-fdtd-$PROGNAME-s" "$FNAME" # & means to background the process
+        "$SCRATCH/2d-fdtd-$PROGNAME-s" "$FNAME"
+        "$SCRATCH/2d-fdtd-$PROGNAME-s" "$FNAME"
+        "$SCRATCH/2d-fdtd-$PROGNAME-s" "$FNAME"
+        "$SCRATCH/2d-fdtd-$PROGNAME-s" "$FNAME"
     done
-    wait # waits for all background processes
-    cp "$SCRATCH/*.npy" "$HOME/"
+    #wait # waits for all background processes
 done
+
